196cdbfc722f0ee3a86deef5f105c0c3e8927b8a
Enes Eren <88148105+thenesern@users.noreply.github.com>
2021-11-13 20:46:03 +0300

    I've fixed some bugs

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-"use strict";
 const searchEl = document.querySelector(".search");
-const form = document.querySelector("#searchform");
+        const form = document.querySelector("#searchform");
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    searchquery = searchEl.value;
-    if (searchquery.length == 0) {
-        return false;
-    } else {
-        window.location = `https://www.google.com/search?q=${searchquery}`;
-    }
-});
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+            searchquery = searchEl.value;
+            if (searchquery.length == 0) {
+                return false;
+            } else {
+                window.location = `https://www.google.com/search?q=${searchquery}`;
+            }
+        });
